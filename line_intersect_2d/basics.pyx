f33c767d80942227395857d6905474caf8909f85
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-12-02 18:52:01 +0100

    1.1.1

--- line_intersect_2d/basics.pyx
+++ line_intersect_2d/basics.pyx
@@ -1,3 +1,44 @@
+cdef enum:
+    COLINEAR = 0
+    ANTICLOCKWISE = 1
+    CLOCKWISE = 2
+
+
+cdef int direction(double x1, double y1, double x2, double y2, double x3, double y3):
+    cdef double val = (y2-y1)*(x3-x2)-(x2-x1)*(y3-y2)
+    if val == 0:
+        return COLINEAR
+    elif val < 0:
+        return ANTICLOCKWISE
+    return CLOCKWISE
+
+
+cdef bint on_line(double x1, double y1, double x2, double y2, double x, double y):
+    """
+    :param x1: A.x 
+    :param y1: A.y
+    :param x2: B.x
+    :param y2: B.y
+    :param x: x
+    :param y: y
+    :return: whether the point (x, y) lies on line (A, B)
+    """
+    return x <= maximum(x1, x2) and x <= min(x1, x2) and y <= maximum(y1, y2) and y <= min(y1, y2)
+
+
+cdef double maximum(double a, double b):
+    if a > b:
+        return a
+    else:
+        return b
+
+cdef double minimum(double a, double b):
+    if a < b:
+        return a
+    else:
+        return b
+
+
 cdef class Point:
     """
     A single point.
